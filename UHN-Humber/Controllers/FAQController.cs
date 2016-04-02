f2a305854d36cf9982b52b9828473eb6091bd6ae
sooahjung <sajung3000@gmail.com>
2016-04-02 17:43:32 -0400

    .test

--- UHN-Humber/Controllers/FAQController.cs
+++ UHN-Humber/Controllers/FAQController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UHN_Humber.Models;
 
 namespace UHN_Humber.Controllers
 {
     public class FAQController : Controller
     {
+        UHNDBContext uc = new UHNDBContext();
         // GET: FAQ
         public ActionResult Index()
         {
