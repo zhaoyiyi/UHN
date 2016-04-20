b23c5938e0a6b06e584b91dcecdf2fd66b1f0fe3
rajdeep90 <rajdeep_jed90@yahoo.com>
2016-04-20 12:54:35 -0400

    add alert pages

---
+++ UHN-Humber/Controllers/AlertDisplayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UHN_Humber.Areas.Admin.Models;
+
+namespace UHN_Humber.Controllers
+{
+    public class AlertDisplayController : Controller
+    {
+        // GET: AlertDisplay
+        public ActionResult Index()
+        {
+            AlertContext alertContext = new AlertContext();
+            List<Alert> alert = alertContext.Employees.ToList();
+            return View(alert);
+
+        }
+    }
+}
